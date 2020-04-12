1b4fd181ad4f83fb6b02c1bbde37477ba7e243ea
dimo <dalmonimo@gmail.com>
2020-04-12 19:04:40 -0300

    Forgot stupid comma

--- config.def.h
+++ config.def.h
@@ -59,7 +59,7 @@ static Key keys[] = {
 
 	{ MODKEY,               XK_q,      killclient,  { 0 } },
 
-	{ MODKEY|Mod1Mask       XK_u,      focusurgent, { 0 } },
+	{ MODKEY|Mod1Mask,      XK_u,      focusurgent, { 0 } },
 	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                    XK_F11,    fullscreen,  { 0 } },
